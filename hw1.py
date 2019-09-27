be611839a155c43006a710a4c8d08742e3a20459
jay841224 <j1360fy6416nb@gmail.com>
2019-09-27 12:24:23 +0800

    add matrix_transform function

--- hw1.py
+++ hw1.py
@@ -4,6 +4,17 @@ def hardlim(array):
     array[array>=0] = 1
     array[array<0] = 0
     return array
+
+#shape(x,) to shape(x, 1)
+def matrix_transform1(array):
+    p_tran = array.reshape(array.shape[0], 1)
+    return p_tran
+
+#shape(x,) to shape(1, x)
+def matrix_transform2(array):
+    tran = array.reshape(1, array.shape[0])
+    return tran
+
 dict = {(0, 0): 1, (0, 1): 7, (1, 0): 6,(1, 1): 0}
 
 P = np.array([[1, 0, 1, 1, 1, 1, 0, 0, 1],\
@@ -21,25 +32,23 @@ error = 0
 #learn
 while not satisfy:
     #transform P for n1
-    p_tran_temp = P[i]
-    p_tran = p_tran_temp.reshape(p_tran_temp.shape[0], 1)
+    p_tran = matrix_transform1(P[i])
     
     n1 = np.dot(W, p_tran)
     A = hardlim(n1)
     #transform T for E
-    t_tran_temp = T[i]
-    t_tran = t_tran_temp.reshape(t_tran_temp.shape[0], 1)
+    t_tran = matrix_transform1(T[i])
 
     E = t_tran - A
     #transform P for new W
-    p_for_final = p_tran_temp.reshape(1, p_tran_temp.shape[0])
+    p_for_final = matrix_transform2(P[i])
     
     W = W + np.dot(E, p_for_final)
     # calculate error
     error += E*E
     i += 1
-    if i>3:
-        if(error[0, 0]==0 and error[1, 0]==0):
+    if i > 3:
+        if(error[0, 0] == 0 and error[1, 0]==0):
             satisfy = True
         i = 0
         error = 0
